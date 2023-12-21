e08c9f896b3f52fe31791aaf8a6c6773ce5d0791
Yogeshkumar1411 <114661825+Yogeshkumar1411@users.noreply.github.com>
2023-12-21 17:15:26 +0530

    factorial recursion
    factorial recursion

---
+++ fact_rec.c
@@ -0,0 +1,20 @@
+#include"header.h"
+int fact(int num)
+{
+	if(num==0)
+	{
+		return 1;
+	}
+	else
+	{
+		return num*fact(num-1);
+	}
+}
+
+
+int main()
+{
+	int num = 5;
+	printf("%d\n",fact(num));
+	return 0;
+}
