5ec393383e79dad5fdd27eb7551a4ad4cf1a2492
Yogeshkumar1411 <114661825+Yogeshkumar1411@users.noreply.github.com>
2023-12-21 17:14:33 +0530

    Palindrome
    Palindrome

---
+++ palindrome.c
@@ -0,0 +1,21 @@
+#include"header.h"
+int main()
+{
+	int num = 1510;
+	int res = 0;
+	int temp = num;
+	while(temp)
+	{
+		int rem = temp%10;
+		res = rem+(res*10);
+		temp = temp/10;
+	}
+	if(num==res)
+	{
+		printf("Palindrome\n");
+	}
+	else
+	{
+		printf("Not Palindrome\n");
+	}
+}
